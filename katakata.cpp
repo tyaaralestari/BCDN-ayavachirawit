4032e418d8ec44f384bdc362b99c0b40d48c34db
tyaaralestari <c030324111@mahasiswa.poliban.ac.id>
2025-07-09 20:16:58 +0800

    tambah tugas string

---
+++ katakata.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string kata_kata;
+
+    cout << "Masukkan kata kata hari ini : " ;
+    cin >> kata_kata ;
+    cout << "Pahammm "<< kata_kata <<" terimakasih";
+}
