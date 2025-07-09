b7a239884d4e2f8c991972a05db8e2e3eb1066f4
tyaaralestari <c030324111@mahasiswa.poliban.ac.id>
2025-07-09 19:40:30 +0800

    upload tugas integer.cpp

---
+++ float.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka1, angka2;
+    cout << "Masukkan pertama : ";
+    cin >> angka1;
+    cout << "Masukkan kedua : ";
+    cin >> angka2;
+
+    int hasil = angka1 + angka2;
+    cout << "Hasil penjumlahan: " << hasil << endl;
+}
